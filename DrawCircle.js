020527afb8b2946ffecc87b473525f616f638520
LeThanh <kzxlmdtn888@gmail.com>
2022-04-11 14:25:45 +0700

    ..

---
+++ DrawCircle.js
@@ -0,0 +1,5 @@
+let circle1 = new Circle(50,'Red');
+let rad = circle1.getRadius();
+let area = circle1.getArea();
+let peri = circle1.getPeri();
+alert('Radius: '+rad + ' Area: '+ area + " Peri: "+ peri);
